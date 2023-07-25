d1feebbea3f9bd75f1e99d54fcf2e1ae9cf73742
r44ph <revaaph15@gmail.com>
2023-07-25 22:36:11 +0700

    added some image

--- Web/routes/web.php
+++ Web/routes/web.php
@@ -15,8 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
