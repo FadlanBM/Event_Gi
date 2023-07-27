22cc929e934bf93dc770114dcf56625f086bffbc
r44ph <revaaph15@gmail.com>
2023-07-27 11:08:08 +0700

    7/27/23 Frontend (Home)

--- Web/resources/views/layouts/app.blade.php
+++ Web/resources/views/layouts/app.blade.php
@@ -8,8 +8,6 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
@@ -33,6 +31,7 @@
             <!-- Page Content -->
             <main>
                 {{ $slot }}
+                
             </main>
         </div>
     </body>
