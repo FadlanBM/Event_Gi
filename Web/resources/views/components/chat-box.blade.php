27100f135a023da0b1df27b3bddcd7bb0c364a2d
r44ph <revaaph15@gmail.com>
2023-07-29 16:17:25 +0700

    asdf

--- Web/resources/views/components/chat-box.blade.php
+++ Web/resources/views/components/chat-box.blade.php
@@ -15,10 +15,27 @@
 
         {{-- Member --}}
         <div class="py-[16px]">
-            <div class="flex flex-col">
+            <div class="flex flex-col gap-y-[16px]">
+                {{-- Natasha Sunny --}}
                 <div class="flex items-center gap-x-2 text-black">
                     <img class="rounded-[4px]" src="/assets/natashaavatar.png" />
                     <p class="text-[12px]">Natasha Sunny</p>
+
+                        <div class="flex items-center gap-x-2">
+                            <div class="w-[0.5px] h-6 bg-[#E7E7E8]"></div>
+                            <div class="w-6 h-6 bg-[#E7E7E8] rounded-full flex items-center justify-center ">
+                                <img src="/assets/icon-microphone-black.svg" />
+                            </div>
+    
+                            <div class="w-6 h-6 bg-[#E7E7E8] rounded-full flex items-center justify-center ">
+                                <img src="/assets/icon-video-minus-black.svg" />
+                            </div>
+                        </div>
+                </div>
+                {{-- Sunny Marwah --}}
+                <div class="flex items-center gap-x-2 text-black">
+                    <img class="rounded-[4px]" src="/assets/natashaavatar.png" />
+                    <p class="text-[12px]">Sunny Marwah</p>
 
                     <div class="pl-2">
                         <div class="flex items-center gap-x-2">
@@ -33,6 +50,43 @@
                         </div>
                     </div>
                 </div>
+                {{-- Syarifah Hinata --}}
+                <div class="flex items-center gap-x-2 text-black">
+                    <img class="rounded-[4px]" src="/assets/natashaavatar.png" />
+                    <p class="text-[12px]">Syarifah Hinata</p>
+
+                    <div class="pl-2">
+                        <div class="flex items-center gap-x-2">
+                            <div class="w-[0.5px] h-6 bg-[#E7E7E8]"></div>
+                            <div class="w-6 h-6 bg-[#E7E7E8] rounded-full flex items-center justify-center ">
+                                <img src="/assets/icon-microphone-black.svg" />
+                            </div>
+    
+                            <div class="w-6 h-6 bg-[#E7E7E8] rounded-full flex items-center justify-center ">
+                                <img src="/assets/icon-video-minus-black.svg" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {{-- Helen Mentari --}}
+                <div class="flex items-center gap-x-2 text-black">
+                    <img class="rounded-[4px]" src="/assets/natashaavatar.png" />
+                    <p class="text-[12px]">Helen Mentari</p>
+
+                    <div class="pl-2">
+                        <div class="flex items-center gap-x-2">
+                            <div class="w-[0.5px] h-6 bg-[#E7E7E8]"></div>
+                            <div class="w-6 h-6 bg-[#E7E7E8] rounded-full flex items-center justify-center ">
+                                <img src="/assets/icon-microphone-black.svg" />
+                            </div>
+    
+                            <div class="w-6 h-6 bg-[#E7E7E8] rounded-full flex items-center justify-center ">
+                                <img src="/assets/icon-video-minus-black.svg" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
 
