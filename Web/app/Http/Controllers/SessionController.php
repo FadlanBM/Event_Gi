4b37236fd9b1ed2b78d64e92ba3d809ba223cda5
Mustleaf <97722578+Mustleaf@users.noreply.github.com>
2023-08-16 11:31:59 +0700

    little

--- Web/app/Http/Controllers/SessionController.php
+++ Web/app/Http/Controllers/SessionController.php
@@ -1,8 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Storage;
 
 class SessionController extends Controller
 {
--- Web/resources/views/layout/this.blade.php
+++ Web/resources/views/layout/this.blade.php
@@ -45,6 +45,10 @@
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Event
                             </a>
+                            <a class="nav-link {{Request::is('event') ? 'active' : ''}}" href="/">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                User
+                            </a>
                         </div>
                     </div>
                     <div class="sb-sidenav-footer">
